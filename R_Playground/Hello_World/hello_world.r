d8d4713da1f1970d2e8754849e6af13743eace1e
Austin Zhang <60564361+Austin6868@users.noreply.github.com>
2023-09-12 17:18:58 -0400

    Update hello_world.r

--- R_Playground/Hello_World/hello_world.r
+++ R_Playground/Hello_World/hello_world.r
@@ -1,2 +1,12 @@
-cat("hello world!")
+print("hello world!")
 
+y <- 5 + 5
+
+print(5 + 5)
+
+for (x in 1:10) {
+  print(paste(x, y), sep = ",")
+}
+# This is a comment
+
+print(paste(class(x), class(y)))
